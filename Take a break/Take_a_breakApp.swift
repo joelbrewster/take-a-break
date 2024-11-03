8f77a5694039821497000878e7f469cf4a7024c9
Joel Brewster <hi@joelbrewster.com>
2024-11-03 20:29:50 +1100

    Window tweaks

--- Take a break/Take_a_breakApp.swift
+++ Take a break/Take_a_breakApp.swift
@@ -8,6 +8,9 @@ struct Take_a_breakApp: App {
         WindowGroup {
             ContentView()
         }
+        .windowStyle(.hiddenTitleBar)
+        .windowResizability(.contentSize)
+        .defaultSize(width: 300, height: 400)
         .commands {
             CommandGroup(replacing: .appInfo) {
                 Button("About Take a break") {
